b214b28b3e76b26fac87014d0fc195a28c08172e
Julia Heloísa <1267022@sga.pucminas.br>
2025-04-16 20:43:06 -0300

    fixing / route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
 // app/page.tsx
 'use client';
 
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { useAuth } from '@/hooks/useAuth';
+
 export default function Home() {
-  return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  );
+  const { user } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user) {
+      router.replace('/all-locations');
+    } else {
+      router.replace('/login');
+    }
+  }, [user, router]);
+
+  return null; // Não renderiza nada enquanto redireciona
 }
